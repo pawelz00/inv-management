3f59b9e58b4e93918b34c22b1ff49e7202194e7c
pawelz00 <pawet.wets@gmail.com>
2025-01-13 20:52:10 +0100

    feat: implement id

--- src/commands/createProductCommand.ts
+++ src/commands/createProductCommand.ts
@@ -1,6 +1,7 @@
 import {db} from "../db";
 
 export function createProductCommand(data: any) {
-    db.data.products.push(data)
+    const lastId = db.data.products[db.data.products.length - 1]?.id || 0
+    db.data.products.push({id: lastId + 1, ...data})
     return data
 }
